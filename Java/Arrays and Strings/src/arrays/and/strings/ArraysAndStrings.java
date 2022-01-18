e6ce9a5a96270f6d22212b533fe51fa90336972e
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-01-18 11:12:45 -0500

    added examples for hashmaps,arraylist and stringbuffer

--- Java/Arrays and Strings/src/arrays/and/strings/ArraysAndStrings.java
+++ Java/Arrays and Strings/src/arrays/and/strings/ArraysAndStrings.java
@@ -5,6 +5,7 @@ package arrays.and.strings;
  *
  * @author Amanuel
  */
+import java.util.*;
 public class ArraysAndStrings {
 
     /**
@@ -13,7 +14,10 @@ public class ArraysAndStrings {
     public static void main(String[] args) {
         // TODO code application logic here
         //System.out.println(check_if_string_has_all_unique_characters("Heo wrld!"));//testing Function 1
-        System.out.println(check_if_string_has_all_unique_characters_without_another_datastructure("Hello world!"));//testing Function 2
+        //System.out.println(check_if_string_has_all_unique_characters_without_another_datastructure("Hello world!"));//testing Function 2
+        //hashmap_example(); //check the hashmap example
+        //arraylist_example();//arraylist example
+        StringBuffer_example(); //stringbuffere example
     }
     
     //Function 1 used if it is allowed to use another datastructure
@@ -49,6 +53,70 @@ public class ArraysAndStrings {
         }
         
         return true;
+        
+    }
+    
+    public static void hashmap_example()
+    {
+        Hashtable<Integer, String> ht1 = new Hashtable<>();
+        ht1.put(10,"a");
+        ht1.put(5,"B");
+        ht1.put(3,"c");
+        
+        
+        //iteration
+        for (Map.Entry<Integer, String> e: ht1.entrySet())
+        {
+            System.out.println(e.getKey()+ " "+e.getValue());
+        }
+        
+        
+        
+    }
+    
+    public static void arraylist_example()
+    {
+        ArrayList<String> al = new ArrayList<>(3);
+        al.add("a");
+        al.add("c");
+        al.add(1,"b");
+        al.add("3");
+        al.set(3,"d");
+        al.add("3");
+        al.add(5,"e");
+        System.out.println("Initial ArrayList "+al);
+        
+        al.remove(4);
+        System.out.println("After remove ArrayList "+al);
+        
+        for (String s:al)
+        {
+            System.out.print(s+" ");
+        }
+        System.out.println();
+        
+        
+    }
+    
+    public static void StringBuffer_example()
+    {
+        StringBuffer s = new StringBuffer("Thisisit");
+        int p = s.length();
+        int q = s. capacity();
+        System.out.println("Length of string Thisisit "+p);
+        System.out.println("Capacity of string s "+q);
+        s.append("!");
+        System.out.println(s);
+        s.insert(4, " ");
+        s.insert(s.length()-1,43.21);
+        System.out.println(s);
+        System.out.println(s.reverse());
+        System.out.println(s.delete(0,5));
+        s.deleteCharAt(5);
+        System.out.println(s);
+        s.replace(5,8,"are");
+        System.out.println(s);
+        
         
     }
     
