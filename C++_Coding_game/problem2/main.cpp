e57bd800c739b7de5a7301059728ce83cfb07a1f
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-03-08 22:06:35 -0500

    Started coding game challenges

---
+++ C++_Coding_game/problem2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    int m;
+    cin >> m; cin.ignore();
+    if(n<=m) cout << n << endl;
+    else cout << m << endl;
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    //cout << "answer" << endl;
+}
