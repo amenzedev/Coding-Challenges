406f7a89740ad600fc8ecbf348c04dbfe55e8de8
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-08-31 09:42:14 -0400

    Stacks2

---
+++ C++_Revision/Stacks2/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<int> postFix;
+    string expression = "524*+7-";
+    int operand1,operand2;
+    for(int i=0;i<expression.length();i++)
+    {
+        if(isdigit(expression[i]))
+        {
+            postFix.push(stoi(expression.substr(i,1)));
+        }
+        else{
+            operand1 = postFix.top();
+            postFix.pop();
+            operand2 = postFix.top();
+            postFix.pop();
+        
+            switch(expression[i])
+            {
+                case '+':
+                    postFix.push(operand1 + operand2);
+                    break;
+                case '-':
+                    postFix.push(operand2 - operand1);
+                    break;
+                case '*':
+                    postFix.push(operand1 * operand2);
+                    break;
+                case '/':
+                    postFix.push(operand2 / operand1);
+                    break;
+
+            }
+        }
+    }
+    cout<<"The answer to "<< expression <<endl;
+    cout<< " is "<<postFix.top()<<endl;
+
+    return 0;
+}
