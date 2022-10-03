47760a734129c14836321df246a80493b5dc6775
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-10-03 11:09:10 -0400

    Problem1_1

---
+++ Java/Problem1_1/README.md
@@ -0,0 +1,6 @@
+# Problem 1.1
+
+Implement an algorithm to determine if a string has all unique characters. 
+What if you cannot use additional data structures?
+
+** ask the interviewer if it is only ASCII code
---
+++ Java/Problem1_1/uniqueCharacters.java
@@ -0,0 +1,37 @@
+package Problem1_1;
+
+import java.util.Scanner;
+
+public class uniqueCharacters {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String input = in.next();
+        
+
+        System.out.println(isUniqueChars(input));
+        
+        
+    }
+
+    public static boolean isUniqueChars(String str)
+    {
+        if(str.length() > 256 ) return false;
+
+        boolean[] char_set = new boolean[256];
+        for(int i=0;i<char_set.length;i++)
+        {
+            
+            if(char_set[str.charAt(i)])
+                return false;
+            char_set[str.charAt(i)]= true;
+            
+
+        }
+
+        return true;
+    }
+    
+}
